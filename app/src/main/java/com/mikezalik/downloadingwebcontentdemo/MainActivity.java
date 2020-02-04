e879b6437feafcebd6910fa57ee022d85da05d6a
mikezalik <mike.zalik@gmail.com>
2020-02-04 08:57:42 -0800

    Adds Download task.

--- app/src/main/java/com/mikezalik/downloadingwebcontentdemo/MainActivity.java
+++ app/src/main/java/com/mikezalik/downloadingwebcontentdemo/MainActivity.java
@@ -2,13 +2,28 @@ package com.mikezalik.downloadingwebcontentdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.i("URL", strings[0]);
+
+            return "Done";
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        task.execute("http://www.github.com/mikezalik");
     }
 }
